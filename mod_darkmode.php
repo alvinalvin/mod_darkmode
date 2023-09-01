10ab01dc89d02dc755ba33846d33d02622dccbc7
Alvin Gil Saldaña <alvin_gil@hotmail.com>
2023-09-01 00:25:30 -0500

    commit 3

---
+++ mod_darkmode.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @copyright	Copyright © 2022 - All rights reserved.
+ * @license		GNU General Public License v2.0
+ * @generator	https://xdsoft/joomla-module-generator/
+ */
+defined('_JEXEC') or die;
+$doc = JFactory::getDocument();
+// Include assets
+$doc->addStyleSheet(JURI::root()."modules/mod_darkmode/assets/css/styles.css");
+$doc->addScript(JURI::root()."modules/mod_darkmode/assets/js/main.js");
+require JModuleHelper::getLayoutPath('mod_darkmode', $params->get('layout', 'default'));
---
+++ tmpl/default.php
@@ -0,0 +1,37 @@
+<?php defined('_JEXEC') or die;
+use Joomla\CMS\Factory;
+$doc = Factory::getDocument();
+JHtml::_('jquery.framework');
+// recibiendo parametros
+$modocolor   = $params->get("modecolor");
+$content  = $params->get("contentcolor");
+
+// estilos en linea
+$style = 'body.dark {
+   background-color:'.$modocolor.';
+   color:'.$content.' ;
+}
+.radio-btn {
+   width: 150px;
+   height: 80px;
+   padding: 10px;
+   background-color: #27173a;
+   border-radius: 50px;
+   cursor: pointer;
+   overflow: hidden;
+}
+.radio-btn .radio-inner {
+   position: relative;
+   width: 60px;
+   height: 60px;
+   background-color: #ffc207;
+   border-radius: 50%;
+   transition: all 0.6s;
+}
+';
+$doc->addStyleDeclaration($style);
+?>
+<!-- etiqueta html para boton -->
+<div id="boton" class="radio-btn">
+        <div class="radio-inner"></div>
+</div>
